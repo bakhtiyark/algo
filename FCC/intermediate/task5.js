8049b351f9ea3ba5caee51b573d67fde255bd129
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-07-09 00:19:56 +0500

    ff

---
+++ FCC/intermediate/task5.js
@@ -0,0 +1,6 @@
+﻿function spinalCase(str) {
+  str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+  return str.toLowerCase().replaceAll(/\s+|\_+/g, "-");
+}
+
+console.log(spinalCase("This Is Spinal Tap"));
