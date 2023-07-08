3045e003f6a5d835eee1d802f923a4401b73ad4f
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-07-08 18:58:38 +0500

    first commit

---
+++ FCC/task1.js
@@ -0,0 +1,11 @@
+﻿function sumAll(arr) {
+  let min = Math.min(arr[0], arr[1]);
+  let max = Math.max(arr[0], arr[1]);
+  let res = 0;
+  for (let i = min; i <= max; i++) {
+    res += i;
+  }
+  return res;
+}
+
+sumAll([1, 4]);
