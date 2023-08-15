be9f397c4ceaf6a7e28497120f0ef867a479f728
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-08-15 22:08:09 +0500

    task 9

---
+++ FCC/base/task9.js
@@ -0,0 +1,12 @@
+﻿function findElement(arr, func) {
+    let num = 0;
+  
+    for (let i = 0; i < arr.length; i++) {
+      num = arr[i];
+      if (func(num)) {
+        return num;
+      }
+    }
+  
+    return undefined;
+  }
